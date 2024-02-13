9079615a10ee70a6406a9cb4023bcf8ac3c452cf
RyadC <r.chair@hotmail.fr>
2024-02-13 10:06:49 +0100

    feat: Add pattern on input value

--- utils.js
+++ utils.js
@@ -1,15 +1,26 @@
 function formattingInputValue(inputValue) {
+  // Enlever les espaces de début et de fin
   let inputValueTrimed = inputValue.trim();
 
+  /*
+  // Renvoie un tableau de la valeur séparée selon les espaces trouvés 
   let arraySplitInputValueTrimed = inputValueTrimed.split(/( )+/);
 
- let inputValueWithoutMultipleSpaces = arraySplitInputValueTrimed.join('');
+  // Forme une string en remplacant les espaces multiples par un seul espace
+  let inputValueWithoutMultipleSpaces = arraySplitInputValueTrimed.join('');
+  */
 
- let firstLetterInCapital = inputValueWithoutMultipleSpaces[0].toUpperCase();
+  // Forme une string en remplacant les espaces multiples par un seul espace
+  let inputValueWithoutMultipleSpaces = inputValueTrimed.replaceAll(/( )+/g, ' ');
 
- let restOfWord = inputValueWithoutMultipleSpaces.slice(1)
+  // Récupère la 1ère lettre pour la mettre en majuscule
+  let firstLetterInCapital = inputValueWithoutMultipleSpaces[0].toUpperCase();
 
- let inputValueFormatted = firstLetterInCapital + restOfWord
+  // Récupère le reste de la string
+  let restOfWord = inputValueWithoutMultipleSpaces.slice(1)
+
+  // Concatène la string entière afin d'avoir une string commençant par une majuscule
+  let inputValueFormatted = firstLetterInCapital + restOfWord
 
   return inputValueFormatted;
 }
