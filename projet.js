abb6fd05b8bd5d94c5dd0900d04ac00a4154e56b
RyadC <r.chair@hotmail.fr>
2024-03-06 12:40:41 +0100

    Add indicator on draggable <li>

--- projet.js
+++ projet.js
@@ -19,12 +19,18 @@ document.addEventListener('DOMContentLoaded', () => {
   // * Initialiser le localStorage
   const store = window.localStorage;
   const KEY_STORE = 'list';
+
+  console.log(store)
   
   /****** DOM ELEMENTS *******/
   const EL_FORM = document.querySelector('.form');
   const EL_INPUT_ADD_ITEM = document.querySelector('#nouvel-item');
   const EL_TEMPLATE = document.querySelector('#template-item');
   const EL_UL = document.querySelector('#liste');
+
+  // ! L'élément doit être crée à l'extérieur de la création du <li> car sinon autant d'indicateurs sont crées alors que nous voulons un unique indicateur pour l'ensemble
+  const EL_INDICATOR = document.createElement('li');
+  EL_INDICATOR.classList.add('indicateur');
 
   /***************************/
   
@@ -46,28 +52,104 @@ document.addEventListener('DOMContentLoaded', () => {
   // * Afficher les items récupérés du localStorage sur la page
   for(const item of reverseArrayOfUserItems) {
     // -> Créer un élément <li> avec les données utilisateur
-    const newLiElement = createLiHtmlItem(EL_TEMPLATE.content, item);
+    const EL_LI = createLiHtmlItem(EL_TEMPLATE.content, item);
 
     // -> Ajouter le <li> en début de liste
-    EL_UL.insertAdjacentElement("afterbegin",newLiElement);
+    EL_UL.insertAdjacentElement("afterbegin",EL_LI);
 
     // -> Ajouter les écouteurs d'évènement au <li>
-      // -> Pour p.nom
-    const EL_P_NOM = newLiElement.querySelector('.nom');
+      // -> Focus sur p.nom
+    const EL_P_NOM = EL_LI.querySelector('.nom');
     focusOn(EL_P_NOM, arrayOfUserItems, store, KEY_STORE);
     
-      // -> Pour p.quantite
-    const EL_P_QUANTITY = newLiElement.querySelector('.quantite');
+      // -> Focus sur p.quantite
+    const EL_P_QUANTITY = EL_LI.querySelector('.quantite');
     focusOn(EL_P_QUANTITY, arrayOfUserItems, store, KEY_STORE);
 
-      // -> Pour le select
-    const EL_SELECT = newLiElement.querySelector('.unite');
+      // -> Change sur le select
+    const EL_SELECT = EL_LI.querySelector('.unite');
     changeOn(EL_SELECT, arrayOfUserItems, store, KEY_STORE);
 
-      // -> Pour la suppression de l'item
-    const EL_DELETE_BTN = document.querySelector('.supprimer');
+      // -> Click sur le btn pour la suppression de l'item
+    const EL_DELETE_BTN = EL_LI.querySelector('.supprimer');
     deleteOn(EL_DELETE_BTN, arrayOfUserItems, store, KEY_STORE);
+
+      // -> Drag du <li>
+    const EL_HANDLE = EL_LI.querySelector('.poignee');
+    EL_HANDLE.addEventListener('mousedown', (e) => {
+      // console.log(e);
+      EL_LI.setAttribute('draggable', 'true');
+    });
+
+    EL_HANDLE.addEventListener('mouseup', (e) => {
+      // console.log(e);
+      // EL_LI.removeAttribute('draggable');
+      // console.log(EL_INDICATOR)
+      EL_INDICATOR.remove();
+
+    });
+
+    EL_LI.addEventListener('dragstart', (e) => {
+      // console.log('dragstart');
+      EL_LI.classList.add('drag-start');
+    })
+    
+
+    EL_LI.addEventListener('dragend', (e) => {
+      // console.log('dragend');
+      EL_LI.removeAttribute('draggable');
+      EL_LI.classList.remove('drag-start')
+      // console.log(EL_INDICATOR)
+      EL_INDICATOR.remove();
+    });
+
+
+
+
+    EL_LI.addEventListener('dragover', (e) => {
+      const isDragLi = EL_LI.classList.contains('drag-start');
+      if(!isDragLi) {
+        const locationOfDrag = e.offsetY; 
+        const middleHeightOfLi = EL_LI.clientHeight / 2;
+        // console.log('middleHeightOfLi : ', middleHeightOfLi);
+        // console.log('locationOfDrag : ', locationOfDrag);
+  
+        // -> Si le drag est au dessous de la moitié de l'élément dragover
+        if(locationOfDrag > middleHeightOfLi) {
+          // -> Y a-t-il un <li> après l'élément dragover ? Si oui, s'agit-il de l'indicateur
+          const isIndicatorPresent = EL_LI.nextElementSibling ? EL_LI.nextElementSibling.classList.contains('indicateur') : false;
+
+          if(!isIndicatorPresent) {
+            EL_LI.insertAdjacentElement('afterend', EL_INDICATOR);
+          }
+          // -> Si le drag est au dessus de la moitié de l'élément dragover
+        } else {
+          // -> Y a-t-il un <li> avant l'élément dragover ? Si oui, s'agit-il de l'indicateur
+          const isIndicatorPresent = EL_LI.previousElementSibling ?  EL_LI.previousElementSibling.classList.contains('indicateur') : false;
+
+          if(!isIndicatorPresent) {
+            EL_LI.insertAdjacentElement('beforebegin', EL_INDICATOR);
+          }
+        }
+      }
+
+     
+
+    })
+
+    
+    
+    
+    
+    EL_UL.addEventListener('dragover', (e) => {
+      e.dataTransfer.dropEffect = 'move';
+      
+    })
+
+
   }
+
+
   
 
 
